a896a72fb06b61ce8ad2af7eb9098c2dbd797ab3
allyboba <ally.martine0807@gmail.com>
2021-01-23 21:02:56 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/solis_calculator/MainActivity.java
@@ -0,0 +1,16 @@
+package mcm.edu.ph.solis_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnadd, btnsub, btndiv, btnmult
